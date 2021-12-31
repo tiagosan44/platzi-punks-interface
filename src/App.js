509a1b85feeb9bc3fe2d761c93708f762bc0a18c
Santiago <tiago.san44@gmail.com>
2021-12-31 17:26:50 -0500

    platzi punks interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './views/home';
+import Punks from './views/punks';
+import Punk from './views/punk';
+import MainLayout from './layouts/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainLayout>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/punks" exact element={<Punks />} />
+        <Route path="/punks/:tokenId" exact element={<Punk />} />
+      </Routes>
+    </MainLayout>
   );
 }
 
